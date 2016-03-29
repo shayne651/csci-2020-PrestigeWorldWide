372d7f08b6138bb47697e5681cf6c1afafc8d403
Shayne Taylor <100552047@oncampus.local>
2016-03-29 18:20:09 -0400

    removing the client and server files so i can re upload them in a more orginised way

--- Assignment02/Test.java
+++
@@ -1,9 +0,0 @@
-class Test
-{
-	public static void main(String[] args) {
-		server testServer = new server();
-		client testClient = new client();
-		testServer.start();
-		testClient.start();
-	}
-}
