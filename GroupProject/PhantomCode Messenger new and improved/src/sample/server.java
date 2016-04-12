87c7c8d1d02e68cd5f770850388d453e7cdada1c
unknown <100525235@UOSL15-R90266WC.oncampus.local>
2016-04-11 20:42:57 -0400

    added more functionality and look to chat window

---
+++ GroupProject/PhantomCode Messenger new and improved/src/sample/server.java
@@ -0,0 +1,108 @@
+package sample;
+
+import java.net.*;
+import java.io.*;
+
+class server extends Thread 
+{
+	private static Socket cSocket = null;
+	private static boolean upload;
+	private static File file;
+	private static int size;
+	private static String fileName;
+	private static String ipA;
+
+	public server(File file)throws Exception
+	{
+		super();
+		upload=true;
+		this.file=file;
+		//the upload was used to tell the server if it is downloading or uploading files, I would have changed it if i had ever gotten dir working but i couldnt so i left it 
+	}
+	
+	public server(String fileName,String ip)throws Exception
+	{
+		super();
+		ip=ipA;
+		upload=true;
+		this.fileName=fileName;
+		size = 200000;
+		//the upload was used to tell the server if it is downloading or uploading files, I would have changed it if i had ever gotten dir working but i couldnt so i left it 
+	}
+	
+	public void run()
+	{
+		if (upload == false)
+		{
+			sendFile();
+		}
+		else 
+		{
+			getFile();	
+		}
+	}
+	public void sendFile()
+	{
+		try
+		{
+			ServerSocket sSocket = new ServerSocket(5555);
+
+			cSocket = sSocket.accept();
+			System.out.println("Connection Accepted");
+			//sending the file from the server to the client
+			System.out.println(file.length());
+			byte[] transferFile = new byte[(int)file.length()];
+			FileInputStream fin = new FileInputStream(file);
+			BufferedInputStream bin = new BufferedInputStream(fin);
+			bin.read(transferFile,0,transferFile.length);
+			OutputStream out = cSocket.getOutputStream();
+			out.write(transferFile,0,transferFile.length);
+			out.flush();
+			System.out.println("File Sent to client Successfully");
+			cSocket.close();
+			sSocket.close();
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	public void getFile()
+	{
+		try 
+		{
+			System.out.println("V");
+			int read;
+			int currentTotal=0;
+			//my local ip used
+			Socket cSocket = new Socket(ipA,4444);
+			System.out.println("Downloading file...");
+			//copying the file from the server to the client's computer 
+			byte [] transferFile = new byte[2000000];
+			InputStream is = cSocket.getInputStream();
+			FileOutputStream fos = new FileOutputStream("copy-"+fileName);
+			BufferedOutputStream out = new BufferedOutputStream(fos);
+			read = is.read(transferFile,0,transferFile.length);
+			currentTotal = read;
+
+			do
+			{
+				read = is.read(transferFile,currentTotal,(transferFile.length-currentTotal));
+				if (read>=0)
+				{
+					currentTotal+=read;
+				}
+			}while (read > -1);
+
+			out.write(transferFile,0,currentTotal);
+			out.flush();
+			out.close();
+			cSocket.close();
+			System.out.println("transfer complete");
+		}
+		catch(Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+}
